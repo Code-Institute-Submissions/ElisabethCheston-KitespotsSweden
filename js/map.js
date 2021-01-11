ff393f54261fb4a4beebfb2206e80f0fc1db548d
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-01-11 02:45:16 +0000

    Add filter function 'E' for kitespot east layer.

--- js/map.js
+++ js/map.js
@@ -1,17 +1,36 @@
- 
+
+//mapboxgl.accessToken = "pk.eyJ1IjoibGlhaGNoZXN0b24iLCJhIjoiY2tpbG5seXE5MGxhYzJ6bXd4Y2xvN2xwMiJ9.efl4PsN0s5YHbu22oEqrlg"
+//import createFilter from "feature-filter-geojson";
+/*
+function geojson_filter(geojson, filter_exp) {
+  var features = geojson.features;
+  geojson.features = [];
+  var geojson_clon = Object.assign({}, geojson);
+  geojson.features = features;
+
+  var filter = createFilter(filter_exp);
+  geojson_clon .features = filter(geojson.features, filter);    
+  return geojson_clon;
+}
+
+var filter = ["all",["==", "windDirection", "S"],];
+
+var filtered_geojson = geojson_filter(kitespots, filter);
+*/
+
+
         // - Basemaps variables - //
-        var hybrid = L.esri.basemapLayer('ImageryClarity');
-        var topographic = L.esri.basemapLayer('Topographic');
-        var streets = L.esri.basemapLayer('Streets');
+    var hybrid = L.esri.basemapLayer('ImageryClarity');
+    var topographic = L.esri.basemapLayer('Topographic');
+    var streets = L.esri.basemapLayer('Streets');
 
         // - Create an on load ESRI basemap - //
-        var map = L.map('map', {
-            center: [62.45,17.45],
-            zoom: 5
-        }),
-        baseImagery = L.layerGroup();
-        L.esri.basemapLayer('ImageryClarity').addTo(map);
-
+    var map = L.map('map', {
+        center: [62.45,17.45],
+        zoom: 5
+    }),
+    baseImagery = L.layerGroup();
+    L.esri.basemapLayer('ImageryClarity').addTo(map);
 
         /* 
         Url references for clustering and adding geoJson to markers in control layer; 
@@ -28,85 +47,66 @@
         */
                 // - Cluster and popups to ALL kitespots - //
 
-        var allSpotsCluster = new L.markerClusterGroup();
-        var allKitespots = L.geoJson(kitespots, {
-            pointToLayer: function (feature, latlng) {
+    var allSpotsCluster = new L.markerClusterGroup();
+    var allKitespots = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
             return L.marker(latlng)
             .bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
-            }
-        });
-        allSpotsCluster.addLayer(allKitespots);
+        }
+    });
+    allSpotsCluster.addLayer(allKitespots);
 
         // - Cluster and popups to kitespots facing SOUTH   - //
-        var southSpotCluster = new L.markerClusterGroup();
-        var kitespotS = L.geoJson(kitespots, {
-            pointToLayer: function (feature, latlng) {
+    var southSpotCluster = new L.markerClusterGroup();
+    var kitespotS = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
             return L.marker(latlng)
             .bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
-            }
-        });
-        southSpotCluster.addLayer(kitespotS);
+        }
+    });
+    southSpotCluster.addLayer(kitespotS);
 
         // - Cluster and popups to kitespots facing WEST - //
-        var westSpotCluster = new L.markerClusterGroup();
-        var kitespotW = L.geoJson(kitespots, {
-            pointToLayer: function (feature, latlng) {
+    var westSpotCluster = new L.markerClusterGroup();
+    var kitespotW = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
             return L.marker(latlng)
             .bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
-            }
-        });
-        westSpotCluster.addLayer(kitespotW);
+        }
+    });
+    westSpotCluster.addLayer(kitespotW);
 
         // - Cluster and popups to kitespots facing NORTH - //
-        var northSpotCluster = new L.markerClusterGroup();
-        var kitespotN = L.geoJson(kitespots, {
-            pointToLayer: function (feature, latlng) {
+    var northSpotCluster = new L.markerClusterGroup();
+    var kitespotN = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
             return L.marker(latlng)
             .bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
-            }
-        });        
-        northSpotCluster.addLayer(kitespotN);
-
-        // - Cluster and popups to kitespots facing EAST - //
-        var eastSpotCluster = new L.markerClusterGroup();
-        var kitespotE = L.geoJson(kitespots, {
-            pointToLayer: function (feature, latlng) {
-            return L.marker(latlng)
-            .bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
-            }
-        });
-        eastSpotCluster.addLayer(kitespotE);
-
-
-/*
-        function geojson_filter(geojson, filter_exp) {
-            let features = geojson.features;
-            geojson.features = [];
-            let geojson_clon = Object.assign({}, geojson);
-            geojson.features = features;
-
-            let filter = createFilter(filter_exp);
-            geojson_clon .features = filter(geojson.features, filter);    
-            return geojson_clon ;
-        }
-        var ffilter = ["allKitespots",["==", "windDirection", "S"],];
-
-        var filtered_geojson = geojson_filter(kitespots , ffilter);
-        /*
-        // - Filter function for kitespots - //
-        var filterKitespots = L.geoJson(null, {
-        onEachFeature: function(feature, layer) {
-        
-            var allSpotsCluster = feature.properties();
-                if      ( windDirection == 'S' ) layer.addTo(kitespotS);
-                else if ( windDirection == 'W' ) layer.addTo(kitespotW);
-                else if ( windDirection == 'N' ) layer.addTo(kitespotN);
-                else if ( windDirection == 'E' ) layer.addTo(kitespotE);
-                else 
-                layer.addTo(statusUnsure);
         }
     });        
-*/
+    northSpotCluster.addLayer(kitespotN);
+
+        // - Cluster and popups to kitespots facing EAST - //
+    var eastSpotCluster = new L.markerClusterGroup();
+    var kitespotE = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .7,
+                color:"red"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature, layer) {
+                feature["properties"]["windDirection"] == "E"
+            },
+            filter: function(feature, layer) {   
+                return (feature.properties.windDirection == "E"
+            );
+        }       
+    });
+    eastSpotCluster.addLayer(kitespotE);
+
+
         // - Control layers - //
         var baseLayers = {
             "Hybrid": hybrid,
@@ -121,10 +121,7 @@
             'Kitespots E': eastSpotCluster
         }
         
-
-
         L.control.layers(baseLayers, overLays).addTo(map);
-
 
 /*
         // - Kitespots from geojson made in GeoJson.io - //
