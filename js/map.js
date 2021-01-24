fa19afd455227ad926410a6507147c4e808ac921
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-01-24 07:54:18 +0000

    Move back the kitespots layers to map js file.

--- js/map.js
+++ js/map.js
@@ -19,39 +19,263 @@
     L.esri.basemapLayer('Topographic').addTo(map);
 
 
-                // - ZOOM FUNCTION FOR OVERLAYS - //
-            //Refrence: https://stackoverflow.com/questions/45286918/leafletjs-dynamically-bound-map-to-visible-overlays
-    var swed = [[68.4,25], [55,10]]; //create variable with SRC coordinate scale
-    map.on('layeradd layerremove', function () {
-        // Create new empty bounds
-        var regionBounds = new L.LatLngBounds();
-        // Iterate the map's layers
-        map.eachLayer(function (layer) {
-            // Check if layer is a featuregroup
-            if (layer instanceof L.FeatureGroup) {
-                // Extend bounds with group's bounds
-                regionBounds.extend(layer.getBounds());
-            }
-        });
-        // Check if bounds are valid (could be empty)
-        if (regionBounds.isValid()) {
-            
-            //function onClick(event) {
-            //    map.zoom([event.latlng])
-           // }
-            // Valid, fit bounds
-            map.fitBounds(regionBounds);
 
-        } else {
-            // Invalid, fit world
-            map.fitBounds(swed);
-        }
+
+        // - Cluster and popups to kitespots North - //
+    var northCluster = new L.markerClusterGroup();
+    var north = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                icon:"pink"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>" + "<a href ='https://www.google.se/maps/@59.3036556,17.9778991,14z'>GET HERE</a>");
+        },
+        onEachFeature: function (feature) {
+            feature["properties"]["label"] == "NORTH"
+        },
+        filter: function(feature) {   
+            return (feature.properties.label == "NORTH");
+        }       
     });
+    northCluster.addLayer(north);
 
 
+        // - Cluster and popups to kitespots North East - //
+    var northeastCluster = new L.markerClusterGroup();
+    var northeast = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                //icon: greenflag.png,
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name 
+            + "</b><br/>" + "Wind Direction: " 
+            + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "NORTHEAST"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "NORTHEAST"
+            );
+        }       
+    });       
+    northeastCluster.addLayer(northeast);
+
+    
+        // - Cluster and popups to kitespots Mid East - //
+    var mideastCluster = new L.markerClusterGroup();
+    var mideast = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "MIDEAST"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "MIDEAST"
+            );
+        }       
+    });       
+    mideastCluster.addLayer(mideast);
 
 
-        // - Control layers - //
+        // - Cluster and popups to kitespots Åland - //
+    var alandCluster = new L.markerClusterGroup();
+    var aland = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "ÅLAND"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "ÅLAND"
+            );
+        }       
+    });       
+    alandCluster.addLayer(aland); 
+    
+
+        // - Cluster and popups to kitespots Gotland - //
+    var gotlandCluster = new L.markerClusterGroup();
+    var gotland = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "GOTLAND"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "GOTLAND"
+            );
+        }       
+    });       
+    gotlandCluster.addLayer(gotland);
+    
+    
+        // - Cluster and popups to kitespots Öland - //
+    var olandCluster = new L.markerClusterGroup();
+    var oland = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "ÖLAND"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "ÖLAND"
+            );
+        }       
+    });       
+    olandCluster.addLayer(oland);
+    
+    
+        // - Cluster and popups to kitespots South East - //
+    var southeastCluster = new L.markerClusterGroup();
+    var southeast = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "SOUTHEAST"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "SOUTHEAST"
+            );
+        }       
+    });       
+    southeastCluster.addLayer(southeast);
+    
+    
+        // - Cluster and popups to kitespots South - //
+    var southCluster = new L.markerClusterGroup();
+    var south = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "SOUTH"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "SOUTH"
+            );
+        }       
+    });       
+    southCluster.addLayer(south);
+    
+    
+        // - Cluster and popups to kitespots south west - //
+    var southwestCluster = new L.markerClusterGroup();
+    var southwest = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "SOUTHWEST"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "SOUTHWEST"
+            );
+        }       
+    });       
+    southwestCluster.addLayer(southwest);
+    
+    
+        // - Cluster and popups to kitespots north west - //
+    var northwestCluster = new L.markerClusterGroup();
+    var northwest = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8,
+                color:"green"
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "NORTHWEST"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "NORTHWEST"
+            );
+        }       
+    });       
+    northwestCluster.addLayer(northwest);
+    
+    
+        // - Cluster and popups to kitespots Vänern - //
+    var vanernCluster = new L.markerClusterGroup();
+    var vanern = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "VÄNERN"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "VÄNERN"
+            );
+        },
+              
+    });       
+    vanernCluster.addLayer(vanern);
+    
+        // - Cluster and popups to kitespots Vättern - //
+    var vatternCluster = new L.markerClusterGroup();
+    var vattern = L.geoJson(kitespots, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                radius:6,
+                opacity: .8
+            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+        },
+            onEachFeature: function (feature) {
+                feature["properties"]["label"] == "VÄTTERN"
+            },
+            filter: function(feature) {   
+                return (feature.properties.label == "VÄTTERN"
+            );
+        },
+              
+    });       
+    vatternCluster.addLayer(vattern);    
+
+
+        // - CONTROL LAYERS - //
+
     var baseLayers = {
             "Hybrid": hybrid,
             "Topographic": topographic,
