493dfa3a845cf6deb924d279cfdde29bcf2d57ee
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-01-24 07:42:53 +0000

    Move search engine to its own js file.

--- js/map.js
+++ js/map.js
@@ -18,65 +18,9 @@
     baseImagery = L.layerGroup();
     L.esri.basemapLayer('Topographic').addTo(map);
 
-
-
-
-
 /*
 
-                // - SEARCH ENGINE - //
 
-    var clusterSpots = L.markerClusterGroup();
-        // - Variable for search source(kitespots) - //
-    var searchSpots = L.geoJson(kitespots, {
-        onEachFeature: function(feature, layer) {
-            var popup = '';
-            if (feature.properties.name) {
-                popup += "<p><b> "+feature.properties.name + "</b><br/>" 
-                + "Wind Direction: " + feature.properties.windDirection;
-            }
-            layer.bindPopup(popup);
-        }
-    });
-        // - Create search engine and place it on the map - //
-    var selector = L.control({
-        position: 'topright',
-        opacity: 0.8,
-        size: 10
-    });
-    selector.onAdd = function(map) {
-        var div = L.DomUtil.create('div', 'list-group-item');
-        div.innerHTML = '<select id = "selectSpot"><option value = "init">KITESPOTS</option></select>';
-        return div;
-    };
-    selector.addTo(map);
-
-        // - Function to browse and choose spots - //
-    searchSpots.eachLayer(function(layer) {
-        var spotChoice = document.createElement("option");
-        spotChoice.innerHTML = layer.feature.properties.label + " - " + layer.feature.properties.name;
-        spotChoice.value = layer._leaflet_id;
-        L.DomUtil.get("selectSpot").appendChild(spotChoice);
-    });
-        // - The selectSpot variable for the DomEvent listener. - //
-    var selectSpot = L.DomUtil.get("selectSpot");
-
-        // - Select kitespot on click - //
-    L.DomEvent.addListener(selectSpot, 'click', function(e) {
-        L.DomEvent.stopPropagation(e);
-    });
-        // - ChangeHandler zooms in on choosen spot with popup.""
-    L.DomEvent.addListener(selectSpot, 'change', changeHandler);
-    function changeHandler(e) {
-        var selected = searchSpots.getLayer(e.target.value);
-            clusterSpots.zoomToShowLayer(selected, function() {
-                selected;
-                //enable.allspotsCluster();
-            })
-        }
-    clusterSpots.addLayer(searchSpots);
-     map.addLayer(clusterSpots);
-*/
 
 
                 // - ZOOM FUNCTION FOR OVERLAYS - //
@@ -119,9 +63,10 @@
                 radius:6,
                 opacity: .1
             })
-            .bindPopup("<p><b> "+feature.properties.name + "</b><br/>" 
-                + "Wind Direction: " + feature.properties.windDirection 
-                + "</p>" + "<a href ='https://www.google.se/maps/@59.3036556,17.9778991,14z'><b> GET HERE </b></a>"); 
+            .bindPopup("<p><b> "+feature.properties.name 
+            + "</b><br/>" + "Wind Direction: " 
+            + feature.properties.windDirection + "</p>" 
+            + "<a href ='https://www.google.se/maps/@59.3036556,17.9778991,14z'><b> GET HERE </b></a>"); 
         },
     });
         allspotsCluster.addLayer(allspots);
@@ -156,7 +101,9 @@
                 radius:6,
                 opacity: .8,
                 color:"green"
-            }).bindPopup("<p><b> "+feature.properties.name + "</b><br/>" + "Wind Direction: " + feature.properties.windDirection + "</p>");
+            }).bindPopup("<p><b> "+feature.properties.name 
+            + "</b><br/>" + "Wind Direction: " 
+            + feature.properties.windDirection + "</p>");
         },
             onEachFeature: function (feature) {
                 feature["properties"]["label"] == "NORTHEAST"
