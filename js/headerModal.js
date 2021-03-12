5e497f82eb50d9b0b301c3359477c5f57a9b8aec
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-03-12 15:22:58 +0000

    Move the logoname link between the header modals.

--- js/headerModal.js
+++ js/headerModal.js
@@ -5,18 +5,23 @@
 
 // Get the modalInfo
 var modalInfo = document.getElementById("modalInfo");
+
 // Get the button that opens the modalInfo
 var btnInfo = document.getElementById("infoButton");
+
 // Get the <span> element that closes the modalInfo
 var spanInfo = document.getElementsByClassName("closeInfo")[0];
+
 // When the user clicks on the button, open the modalInfo
 btnInfo.onclick = function () {
     modalInfo.style.display = "block";
 }
+
 // When the user clicks on <span> (x), close the modalInfo
 spanInfo.onclick = function () {
     modalInfo.style.display = "none";
 }
+
 
 // - ATTRIBUTE MODAL - //
 // Get the modalAttribute
